22b707c78a9b202a8be8c2aa87117585882e9692
Andrea Tarifa Narváez <tarifandrea@alumnos.cei.es>
2023-04-29 20:43:28 +0200

    documentation of files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
+/* Evento añadido al cargar la página para ejecutar el método de añadir el año del 
+copyright al footer i la función encargada del menú de navegación en versión mobile */
 window.addEventListener("load", () => {
   getYearCopy();
   initMobileMenu();
 });
 
+// Función que coge el año actual de la fecha y lo pone dentro del elemento con el id "currentYear"
 function getYearCopy() {
   const year = new Date().getFullYear();
   document.getElementById("currentYear").textContent = year;
 }
 
+/* Función que se encarga de coger todos los elementos del menú mobile y añadir 
+un evento de click al botón del menú para ejecutar el método "toggleMenu" */
 function initMobileMenu() {
+  // Elementos del menú para añadir o quitar clases CSS
   const menu = document.querySelector(".menu-mobile");
   const hamburger = document.querySelector(".hamburger");
   const closeIcon = document.querySelector(".closeIcon");
   const menuIcon = document.querySelector(".menuIcon");
 
+  /* Funcion que comprueba si el menú tiene la clase CSS "showMenu", en caso de tenerla 
+  se la quita y oculta el icono de cerrado y en caso de no tenerla se la pone */
   function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
       menu.classList.remove("showMenu");
@@ -26,5 +34,6 @@ function initMobileMenu() {
     }
   }
 
+  // Añade el evento de click al botón del menú mobile
   hamburger.addEventListener("click", toggleMenu);
 }
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,16 +1,23 @@
+// Valor inicial del índice
 var slideIndex = 1;
 
+/* Evento añadido al cargar la página para ejecutar el método de pasar la imagenes del carrousel 
+y hacer que se ejecute cada tres segundos la función de pasar la imagen */
 window.addEventListener("load", () => {
   showSlides(slideIndex);
+
   setInterval(function () {
     plusSlides(1);
   }, 3000);
 });
 
+// Función para sumar o restar un índice al carrousel y así ir hacia delante o hacia atrás
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
+/* Función que dado un índice va pasando de manera automática los slides del carrousel 
+mostrando el siguiente y ocultando el anterior */
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
